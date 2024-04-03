de39482de6a10ad470121aa214d69032b0e76de7
Hugues Boisdon <hboisdon@protonmail.com>
2024-04-03 11:17:34 +0200

    faux Raspberry pour les tests

--- config_gpio.py
+++ config_gpio.py
@@ -1,3 +1,17 @@
+import sys
+
+# La plateforme du RaspberryPI 3
+targetPlatform = 'linux2' 
+
+# Si on lance ce code sur une autre plateforme, on adapte pour pouvoir tester l'implementation
+if (sys.platform != targetPlatform):
+    import fake_rpi
+
+    sys.modules['RPi'] = fake_rpi.RPi     # Faux RPi
+    sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO # Faux GPIO
+
+
+
 def initialize_gpio():
     """
     Paramètres GPIO généraux.
