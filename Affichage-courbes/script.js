4ed288475e65285054b276c0793f698172a07a1c
momo3213 <morgane.hayotte@cpe.fr>
2024-05-07 11:35:55 +0200

    config chart

---
+++ Affichage-courbes/script.js
@@ -0,0 +1,20 @@
+const barCanvas= document.getElementById("barcanvas");
+console.log("Live Server fonctionne correctement et les fichiers sont bien reliés !");
+const barChart = new Chart(barCanvas, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+        y: {
+            beginAtZero: true
+        }
+        }
+    }
+    });
