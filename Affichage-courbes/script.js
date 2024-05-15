bc061a8557af998a27976fc75bc18911a83dd2a1
momo3213 <morgane.hayotte@cpe.fr>
2024-05-15 09:10:39 +0200

    workspace

--- Affichage-courbes/script.js
+++ Affichage-courbes/script.js
@@ -1,20 +1,20 @@
-const barCanvas= document.getElementById("barCanvas");
+const ctx = document.getElementById('barCanvas');
 console.log("Live Server fonctionne correctement et les fichiers sont bien reliés !");
-const barChart = new Chart(barCanvas, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-        y: {
-            beginAtZero: true
-        }
-        }
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
     }
-    });
+  }
+});
