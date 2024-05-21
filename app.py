334922063af9338233da1583cbac6953b9811a5b
Hugues Boisdon <hboisdon@protonmail.com>
2024-05-21 08:31:22 +0200

    presque responsive

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template, request, url_for, jsonify, Response
+import subprocess
 
 app = Flask(__name__)
 
+# Routing
+
 @app.route('/')
 def mainPage():
-   return render_template('main.html')
+   return render_template('index.html')
 
 @app.route('/info')
 def infoPage():
    if request.method == "GET":
       return render_template("info.html")
    
-@app.route('/data')
+@app.route('/navdata')
 def dataPage():
    if request.method == "GET":
       return render_template("data.html")
@@ -21,6 +24,33 @@ def receiveMoveCommand():
     data = request.get_json() 
     print(f'received Command: {data}\n')
     return jsonify(result=data)
+ 
+
+@app.route('/video_feed')
+def video_feed():
+    # MIME type peut varier selon le format de sortie de votre flux vidéo
+    return Response(gen_frames(), mimetype='video/h264')
+
+
+
+
+
+
+#video feed method
+def gen_frames():  
+   # Commande libcamera-vid pour capturer la vidéo
+   # Utilisation de `-t 0` pour un flux continu, `-o -` pour sortir sur stdout
+   cmd = "libcamera-vid -t 0 -o -"
+   p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+
+   while True:
+      # Lire le flux de stdout de libcamera-vid
+      data = p.stdout.read(1024)
+      if not data:
+         break
+      yield (data)
+
+
 
 if __name__ == '__main__':
    app.run()
